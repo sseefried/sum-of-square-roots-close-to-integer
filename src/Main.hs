1a550ff37ba80f258851ebf9204f0fdd959440b5
Sean Seefried <sean@seefried.com.au>
2021-06-11 14:31:19 +1000

    Improve code

--- src/Main.hs
+++ src/Main.hs
@@ -1,54 +1,71 @@
 module Main where
 
 import Data.List
+import Data.BigDecimal
+import Data.BigFloating
+import Debug.Trace
 
+
+ctx :: MathContext
+ctx = (HALF_UP, Just 0)
+
+places = 10
+
+sqrtPrec :: MathContext
+sqrtPrec = (HALF_UP, Just places)
 
 main :: IO ()
 main = do
   let result = test
-  mapM_ (\x -> putStrLn $ show x) result
+  mapM_ (\x -> putStrLn $ show x) $ map (\(x,y,z,_) -> (x,y,z)) $ result
   putStrLn $ "epsilon = " ++ show epsilon
   let numResults = length result
   putStrLn $ "number of pairs = " ++ show numResults
-  let manyNines = filter (\(_,_,sum) -> abs (toFractional (round sum)) > abs sum) result
+  let manyNines = filter (\(_,_,_,sum) -> abs (roundBD sum ctx) > abs sum) result
   let numManyNines = length manyNines
   putStrLn $ "number of pairs whose sum is just below = " ++ show (length manyNines)
   putStrLn $ "percentage = " ++ show ((fromIntegral numManyNines)/(fromIntegral numResults) * 100.0)
 
-
-toFractional :: Integer -> Double
-toFractional = fromIntegral
+toFractional :: Integer -> BigDecimal
+toFractional n = fromInteger n
 
 -- search numbers less than maxNum
-maxNum = 1000
+maxNumX = 500
+maxNumY = 500
 
 -- Find sums this close to an integer
-epsilon = 0.00001
+epsilon :: BigDecimal
+epsilon = 0.0001
 
 nonIntegerSqrt :: Integer -> Bool
-nonIntegerSqrt n = toFractional (round s) /= s
+nonIntegerSqrt n = roundBD s ctx  /= s
   where
-    s = sqrt (toFractional n)
+    s = mySqrt n
 
 -- Tests whether sqrt n + sqrt m (where n and m aren't
 -- perfect squares) is close to an integer
 testPair n m =
-  (nonIntegerSqrt n && nonIntegerSqrt m && d < epsilon && abs (m - n) /= 2 && n /= m,
-  (n, m, d)  )
+  (abs (m - n) /= 2 && n /= m && d < epsilon && notPerfect ,
+  (n, m, v, d)  )
   where
-    d = abs (v - toFractional (round v))
-    v = sqrtSum n m - 0.25
+    sqrtN = mySqrt n
+    sqrtM = mySqrt m
+
+    d = abs (v - roundBD v ctx)
+    v = sqrtN + sqrtM
+    notPerfect = d > BigDecimal 1 places
 
 testList = test' 1 1
   where
-    test' n m | n < maxNum = testPair n m : test' (n+1) m
-    test' n m | m < maxNum = testPair n m : test' 1 (m + 1)
+    test' n m | n < maxNumX = testPair n m : test' (n+1) m
+    test' n m | m < maxNumY = trace (show m ++ "/" ++ show maxNumY) $ testPair n m : test' 1 (m + 1)
     test' _ _ | otherwise = []
 
 test = map getPair $ sortBy compareD $ filter fst testList
   where
     compareD a b = compare (getD a) (getD b)
-    getD (_, (_, _, d)) = d
-    getPair (_, (x, y,_)) = (x,y, sqrtSum x y)
+    getD (_, (_, _, _, d)) = d
+    getPair (_, (x, y, v, _)) = (x,y, toString v, v)
 
-sqrtSum x y = sqrt (toFractional x) + sqrt (toFractional y)
+mySqrt n = sqr (toFractional n) sqrtPrec
+
